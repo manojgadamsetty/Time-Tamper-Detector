da2ae9e0638f4a065a678f3c2a8de3e50d912060
Manoj Gadamsetty <manojgadamsetty@gmail.com>
2025-07-18 19:52:54 +0530

    Initial Commit

---
+++ Time Tamper Detector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time Tamper Detector
+//
+//  Created by Manoj Gadamsetty on 18/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time Tamper Detector/Time_Tamper_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_Tamper_DetectorApp.swift
+//  Time Tamper Detector
+//
+//  Created by Manoj Gadamsetty on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Time_Tamper_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
